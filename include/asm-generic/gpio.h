02a2df4c4be1b52fe735f31caf8bb233c9cc78f0
Sami Kantoluoto <sami.kantoluoto@embedtronics.fi>
2010-09-18 21:36:50 +0300

    gpiolib - added functions to get/set multiple I/O pins at a time - gpio_get_values() and gpio_set_values()

--- include/asm-generic/gpio.h
+++ include/asm-generic/gpio.h
@@ -57,8 +57,10 @@ struct device_node;
  * @direction_input: configures signal "offset" as input, or returns error
  * @get: returns value for signal "offset"; for output signals this
  *	returns either the value actually sensed, or zero
+ * @get_multiple: returns values of multiple signals starting at "offset";
  * @direction_output: configures signal "offset" as output, or returns error
  * @set: assigns output value for signal "offset"
+ * @set_multiple: assigns values for multiple outputs starting at signal "offset"
  * @to_irq: optional hook supporting non-static gpio_to_irq() mappings;
  *	implementation may not sleep
  * @dbg_show: optional routine to show contents in debugfs; default code
@@ -101,6 +103,10 @@ struct gpio_chip {
 						unsigned offset);
 	int			(*get)(struct gpio_chip *chip,
 						unsigned offset);
+	int			(*get_multiple)(struct gpio_chip *chip,
+						unsigned offset,
+						unsigned count,
+						int *values);
 	int			(*direction_output)(struct gpio_chip *chip,
 						unsigned offset, int value);
 	int			(*set_debounce)(struct gpio_chip *chip,
@@ -108,7 +114,10 @@ struct gpio_chip {
 
 	void			(*set)(struct gpio_chip *chip,
 						unsigned offset, int value);
-
+	void			(*set_multiple)(struct gpio_chip *chip,
+						unsigned offset,
+						unsigned count,
+						const int *value);
 	int			(*to_irq)(struct gpio_chip *chip,
 						unsigned offset);
 
